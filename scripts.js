4fef4d93510d075656d9c045b2b9223dcf813619
stefaniRuiz <151537811+stefaniRuiz@users.noreply.github.com>
2024-11-04 14:51:34 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,43 @@
+/*!
+* Start Bootstrap - The Big Picture v5.0.6 (https://startbootstrap.com/template/the-big-picture)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+btConsulta.addEventListener('click', listaMonstros)
+
+function listaMonstros() { 
+let nome = txNome.value;
+txNome.value = ''
+
+fetch('arrayJson')// faz o pedido assícrono
+  .then( res => res.json())
+  .then( listaCompleta => { 
+      const listaFiltrada = listaCompleta.filter(monstro =>
+        monstro.nome.toUpperCase().includes(nome.toUpperCase())
+    )
+
+    // apaga do HTML a resposta anterior
+    corpoTabelaGM.innerHTML = ""
+
+    if(listaFiltrada.length > 0) {
+      listaFiltrada.forEach(monstro => {
+        let linha = document.createElement('tr')
+        linha.innerHTML =`
+            
+            
+            <td>${monstro.descricao}</td>
+          
+            `
+            corpoTabelaGM.appendChild(linha)
+  
+            imgGM.src = monstro.img
+      })
+    
+    } else {
+      // Caso a lista filtrada esteja vazia, significa que não encontramos nenhum nome
+      alert('Monstro não encontrado! Tente um nome da Lista de Monstros')
+    }
+  })
+}
