1da28a9e64ed6119cb7adf0c2f277511ffd91df0
stefaniRuiz <151537811+stefaniRuiz@users.noreply.github.com>
2024-11-04 16:24:18 -0300

    script.js certo

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,59 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+/*--btConsulta.addEventListener('click', listaMonstros)
+
+function listaMonstros() { 
+let nome = txNome.value;
+txNome.value = ''
+
+fetch('arrayJson')// faz o pedido assícrono
+  .then( res => res.json())
+  .then( listaCompleta => { 
+      const listaFiltrada = listaCompleta.filter(monstro =>
+        monstro.nome.toUpperCase().includes(nome.toUpperCase())
+    )
+
+    // apaga do HTML a resposta anterior
+    corpoTabelaGM.innerHTML = ""
+
+    if(listaFiltrada.length > 0) {
+      listaFiltrada.forEach(monstro => {
+        let linha = document.createElement('tr')
+        linha.innerHTML =`
+            
+            
+            <td>${monstro.descricao}</td>
+          
+            `
+            corpoTabelaGM.appendChild(linha)
+  
+            imgGM.src = monstro.img
+      })
+    
+    } else {
+      // Caso a lista filtrada esteja vazia, significa que não encontramos nenhum nome
+      alert('Monstro não encontrado! Tente um nome da Lista de Monstros')
+    }
+  })
+}
+*/
+
+fetch('arrayJson')
+.then( resposta => {
+  return resposta.json()
+})
+.then(dados =>{
+  dados.forEach(monstro=>{    
+    let opMonstro = document.createElement('option')
+    opMonstro.innerText = monstro.nome
+    opMonstro.value = monstro.nome    
+    listaDeMonstros.appendChild(opMonstro)
+  })
+})
+
+
+
 btConsulta.addEventListener('click', listaMonstros)
 
 function listaMonstros() { 
@@ -15,23 +68,29 @@ fetch('arrayJson')// faz o pedido assícrono
   .then( res => res.json())
   .then( listaCompleta => { 
       const listaFiltrada = listaCompleta.filter(monstro =>
-      monstro.nome.toUpperCase().includes(nome.toUpperCase())
-
+        monstro.nome.toUpperCase().includes(nome.toUpperCase())
     )
+
     // apaga do HTML a resposta anterior
     corpoTabelaGM.innerHTML = ""
 
-    listaFiltrada.forEach(monstro => {
-      let linha = document.createElement('tr')
-      linha.innerHTML =`
+    if(listaFiltrada.length > 0) {
+      listaFiltrada.forEach(monstro => {
+        let linha = document.createElement('tr')
+        linha.innerHTML =`
+            
+            
+            <td>${monstro.descricao}</td>
           
-          
-          <td>${monstro.descricao}</td>
-        
-          `
-          corpoTabelaGM.appendChild(linha)
-
-          imgGM.src = monstro.img
-    })
+            `
+            corpoTabelaGM.appendChild(linha)
+  
+            imgGM.src = monstro.img
+      })
+    
+    } else {
+      // Caso a lista filtrada esteja vazia, significa que não encontramos nenhum nome
+      alert('Monstro não encontrado! Tente um nome da Lista de Monstros')
+    }
   })
 }
